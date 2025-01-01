c0078d595f46c9c5d210b36ac70b0df83190dfbf
ahmadalbaqi <ahmadalbaqi03@gmail.com>
2025-01-01 21:39:51 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const toggleButton = document.getElementById('toggle-btn');
-const sidebar = document.getElementById('sidebar');
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
 
 function toggleSidebar() {
-    sidebar.classList.toggle('close');
-    toggleButton.classList.toggle('rotate');
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
 
-    closeAllSubMenu()
+    closeAllSubMenus()
 }
 
 function toggleSubMenu(button) {
 
     if(!button.nextElementSibling.classList.contains('show')) {
-        closeAllSubMenu()  
+        closeAllSubMenus()  
     }
 
     button.nextElementSibling.classList.toggle('show')
     button.classList.toggle('rotate')
 
     if(sidebar.classList.contains('close')){
-        sidebar.classList.toggle('close');
-        toggleButton.classList.toggle('rotate');
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
     }
 }
 
-function closeAllSubMenu() {
+function closeAllSubMenus() {
     Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
-        ul.classList.remove('show');
-        ul.previousElementSibling.classList.remove('rotate');
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
     })
 }
